b87598bb6a615d79f81ba377a15e2b09462f473a
whage <whage@freemail.hu>
2019-06-05 08:26:09 +0200

    trying python lib: found how to run a search query

--- main.py
+++ main.py
@@ -6,8 +6,19 @@ auth_token = None
 db_url = "https://mikerhodes.cloudant.com"
 
 # Note: login without credentials requires admin_party=True.
-# This is not documented anywhere - found it in source, parent class, random docs
+# Couldn't find any documentation on this - found it in source, parent class, random docs
 # https://guide.couchdb.org/draft/security.html
 client = Cloudant(user, auth_token, admin_party=True, url=db_url, connect=True)
 
 airportdb = client["airportdb"]
+
+#print("a0487237c4362b941f7332d7eb768ba0" in airportdb)
+
+
+#for document in airportdb:
+#    print(document)
+
+#print(type(airportdb))
+
+# No docs for this either, found by looking at source, searching for "search"
+print(airportdb.get_search_result("view1", "geo", q="lon:[0 TO 30] AND lat:[0 TO 5]"))
