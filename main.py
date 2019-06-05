f3663121df87ed020e995b2415fc246ef32087f6
whage <whage@freemail.hu>
2019-06-05 22:13:11 +0200

    implement basic distance calculation and sorting, need to tidy up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from math import radians, cos, sin, asin, sqrt
 from cloudant.client import Cloudant
-
+import json
 
 user = None
 auth_token = None
@@ -12,13 +13,49 @@ client = Cloudant(user, auth_token, admin_party=True, url=db_url, connect=True)
 
 airportdb = client["airportdb"]
 
-#print("a0487237c4362b941f7332d7eb768ba0" in airportdb)
+# TODO: read from user input
+box_lon = (0, 10)
+box_lat = (0, 10)
 
-
-#for document in airportdb:
-#    print(document)
-
-#print(type(airportdb))
+# note: no need to sanitize, the query will only read the db
+query = 'lon:[{} TO {}] AND lat:[{} TO {}]'.format(box_lon[0], box_lon[1], box_lat[0], box_lat[1])
 
 # No docs for this either, found by looking at source, searching for "search"
-print(airportdb.get_search_result("view1", "geo", q="lon:[0 TO 30] AND lat:[0 TO 5]"))
+airports_within_bounds = airportdb.get_search_result("view1", "geo", q=query).get("rows", [])
+
+# Spec states: "sorted by distance", doesn't state distance from where.
+# We'll calculate distance from the center of the bounding box using the
+# Haversine formula
+def haversine(lon1, lat1, lon2, lat2):
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    earth_mean_radius = 6371
+
+    # haversine formula
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = earth_mean_radius
+    return c * r
+
+def add_distance(airport):
+    box_center_lon = (box_lon[0] + box_lon[1]) / 2
+    box_center_lat = (box_lat[0] + box_lat[1]) / 2
+
+    airport["distance"] = haversine(
+        box_center_lon,
+        box_center_lat,
+        airport["fields"]["lon"],
+        airport["fields"]["lat"]
+    )
+
+    return airport
+
+def get_distance(airport):
+    return airport["distance"]
+
+airports_with_distance = list(map(add_distance, airports_within_bounds))
+airports_with_distance.sort(key=get_distance)
+
+print(json.dumps(airports_with_distance))
